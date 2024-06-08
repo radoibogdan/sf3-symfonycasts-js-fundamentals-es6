b2d740b5c70e374f6b07d81d6cbdf503dd6dcf3a
Bogdan Radoi <radoi.office@gmail.com>
2024-06-08 19:11:50 +0200

    NodeJs - How to execute JS on the Server

---
+++ play-with-nodejs.js
@@ -0,0 +1,10 @@
+// execute this file using cmd
+// node play-with-nodejs.js
+
+var aNumber = 10;
+
+setTimeout(() => {
+    console.log(aNumber);
+}, 2000);
+
+console.log('Waiting for the timeout function to execute (2sec).')
